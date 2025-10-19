e9ab44d2f1deb1fa926741825c10be5da6aedba4
comptegitetudiant-blip <comptegitetudiant@gmail.com>
2025-10-19 22:02:13 +0200

    Commit final du projet

--- src/main/java/com/example/exercice/Navigator.java
+++ src/main/java/com/example/exercice/Navigator.java
@@ -5,8 +5,12 @@ import javafx.stage.Stage;
 
 public class Navigator {
     private static Stage stage;
-    public static void init(Stage primary){ stage = primary; }
-    public static void goTo(Scene scene, String title){
+
+    public static void init(Stage primary) {
+        stage = primary;
+    }
+
+    public static void goTo(Scene scene, String title) {
         stage.setScene(scene);
         stage.setTitle(title);
         stage.show();
